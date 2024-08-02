9cc9f8e422a52cbfccf79efba07d6dfeb5d35165
perrwa <7255333+perrwa@users.noreply.github.com>
2024-08-01 17:22:33 -0700

    Rename custom keycodes

--- keyboards/crkbd/rev1/keymaps/perrwa/keymap.c
+++ keyboards/crkbd/rev1/keymaps/perrwa/keymap.c
@@ -42,14 +42,14 @@ enum custom_keycodes {
 
     SG_LBRC,
     SG_RBRC,
-    LGUI_GRV,
+    CMD_GRV,
     RGUI_ENT
 };
 
 // Custom keycodes for keymap shorthand
-#define SG_LBRC     SGUI(KC_LBRC)
-#define SG_RBRC     SGUI(KC_RBRC)
-#define LGUI_GRV    LGUI(KC_GRV)
+#define SG_LBRC     SCMD(KC_LBRC)
+#define SG_RBRC     SCMD(KC_RBRC)
+#define CMD_GRV     LCMD(KC_GRV)
 
 #define RGUI_ENT    RGUI_T(KC_ENT)
 
@@ -75,7 +75,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   //,-----------------------------------------------------.                    ,-----------------------------------------------------.
        KC_GRV,    KC_1,    KC_2,    KC_3,    KC_4,    KC_5,                         KC_6,    KC_7,    KC_8,    KC_9,    KC_0, KC_BSLS,
   //|--------+--------+--------+--------+--------+--------|                    |--------+--------+--------+--------+--------+--------|
-       SYSNUM,   KC_NO,   KC_NO, SG_LBRC, SG_RBRC,LGUI_GRV,                        KC_NO, KC_MINS,  KC_EQL, KC_LBRC, KC_RBRC,  SYSNUM,
+       SYSNUM,   KC_NO, CMD_GRV, SG_LBRC, SG_RBRC,   KC_NO,                        KC_NO, KC_MINS,  KC_EQL, KC_LBRC, KC_RBRC,  SYSNUM,
   //|--------+--------+--------+--------+--------+--------|                    |--------+--------+--------+--------+--------+--------|
       KC_TRNS,   KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,                        KC_F6,   KC_F7,   KC_F8,   KC_F9,  KC_F10, KC_TRNS,
   //|--------+--------+--------+--------+--------+--------+--------|  |--------+--------+--------+--------+--------+--------+--------|
