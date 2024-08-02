9719ae2c68d2a66f8e34b94f5a986ff68ec67375
perrwa <7255333+perrwa@users.noreply.github.com>
2024-08-01 17:03:06 -0700

    Remove QMK boot/eeprom

--- keyboards/crkbd/rev1/keymaps/perrwa/keymap.c
+++ keyboards/crkbd/rev1/keymaps/perrwa/keymap.c
@@ -84,11 +84,11 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   ),
   [_NAVCON] = LAYOUT(
   //,-----------------------------------------------------.                    ,-----------------------------------------------------.
-      QK_BOOT,   KC_NO, KC_MUTE, KC_VOLD, KC_VOLU,   KC_NO,                      KC_HOME, KC_PGDN, KC_PGUP,  KC_END,   KC_NO,  KC_DEL,
+        KC_NO,   KC_NO, KC_MUTE, KC_VOLD, KC_VOLU,   KC_NO,                      KC_HOME, KC_PGDN, KC_PGUP,  KC_END,   KC_NO,  KC_DEL,
   //|--------+--------+--------+--------+--------+--------|                    |--------+--------+--------+--------+--------+--------|
       KC_TRNS,   KC_NO, KC_MRWD, KC_MPLY, KC_MFFD,   KC_NO,                      KC_LEFT, KC_DOWN,   KC_UP, KC_RGHT,   KC_NO, KC_TRNS,
   //|--------+--------+--------+--------+--------+--------|                    |--------+--------+--------+--------+--------+--------|
-      KC_TRNS,   KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_NO,                        KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_NO,  EE_CLR,
+      KC_TRNS,   KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_NO,                        KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_NO,
   //|--------+--------+--------+--------+--------+--------+--------|  |--------+--------+--------+--------+--------+--------+--------|
                                           KC_TRNS,  SYSNUM, KC_TRNS,    KC_TRNS,  SYSNUM, KC_TRNS
   //                                    `--------------------------'  `--------------------------'
