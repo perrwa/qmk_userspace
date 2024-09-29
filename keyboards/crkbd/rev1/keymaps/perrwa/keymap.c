d51b35a669453c2e5c8683368ab755c2ea171c70
perrwa <7255333+perrwa@users.noreply.github.com>
2024-09-29 07:37:01 -0700

    Remove GUI/GRV

--- keyboards/crkbd/rev1/keymaps/perrwa/keymap.c
+++ keyboards/crkbd/rev1/keymaps/perrwa/keymap.c
@@ -35,7 +35,6 @@ enum custom_keycodes {
 
     _SGLBRC,
     _SGRBRC,
-    _GUIGRV,
     _GUIENT,
 
     _LWRSPC,
@@ -47,7 +46,6 @@ enum custom_keycodes {
 // Custom keycodes for keymap shorthand
 #define _SGLBRC     SGUI(KC_LBRC)
 #define _SGRBRC     SGUI(KC_RBRC)
-#define _GUIGRV     LGUI(KC_GRV)
 
 #define _GUIENT     LGUI_T(KC_ENT)
 #define _CTLENT     RCTL_T(KC_ENT)
@@ -87,7 +85,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   //,-----------------------------------------------------.                    ,-----------------------------------------------------.
        KC_GRV,    KC_1,    KC_2,    KC_3,    KC_4,    KC_5,                         KC_6,    KC_7,    KC_8,    KC_9,    KC_0, KC_BSLS,
   //|--------+--------+--------+--------+--------+--------|                    |--------+--------+--------+--------+--------+--------|
-      _MO_NUM,   KC_NO, _GUIGRV, _SGLBRC, _SGRBRC,   KC_NO,                        KC_NO, KC_MINS,  KC_EQL, KC_LBRC, KC_RBRC,   KC_NO,
+      _MO_NUM,   KC_NO,   KC_NO, _SGLBRC, _SGRBRC, KC_LBRC,                      KC_RBRC, KC_MINS,  KC_EQL,   KC_NO,   KC_NO,   KC_NO,
   //|--------+--------+--------+--------+--------+--------|                    |--------+--------+--------+--------+--------+--------|
       _______,   KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,                        KC_F6,   KC_F7,   KC_F8,   KC_F9,  KC_F10, _______,
   //|--------+--------+--------+--------+--------+--------+--------|  |--------+--------+--------+--------+--------+--------+--------|
